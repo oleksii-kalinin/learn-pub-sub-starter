685d8b315daa4fa0820f95f4706bf45c1f9ca06a
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-04 15:36:33 +0100

    Add basic connection and pubsub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 
 func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@192.168.1.20:5672/")
+	FailOnError(err, "Failed to connect to AMQP")
+	defer func(conn *amqp.Connection) {
+		err = conn.Close()
+		FailOnError(err, "Unable to close AMQP connection")
+	}(conn)
+
+	log.Println("Connection to AMQP ok")
+
+	mq, err := conn.Channel()
+	FailOnError(err, "Error opening channel")
+
+	err = pubsub.PublishJSON(mq, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+
 	fmt.Println("Starting Peril server...")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	log.Println("Exiting")
 }
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,22 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	jsonVal, err := json.Marshal(val)
+	if err != nil {
+		log.Panicf("Unable to marshal to json: %s", err)
+	}
+
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        jsonVal,
+	})
+	return err
+}
