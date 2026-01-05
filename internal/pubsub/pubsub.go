22027056be5222c49f0e47a6bad680df8529cab7
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-05 20:32:17 +0100

    Update pubsub.go

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -28,6 +28,34 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	return err
 }
 
+func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType, handler func(T)) error {
+	channel, _, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
+	if err != nil {
+		log.Println("unable to bind")
+		return err
+	}
+
+	consumer, err := channel.Consume(queueName, "", false, false, false, false, nil)
+	if err != nil {
+		log.Println("unable to create consumer")
+		return err
+	}
+	go func() {
+		for msg := range consumer {
+			var v T
+			err = json.Unmarshal(msg.Body, &v)
+			if err != nil {
+				log.Println("unable to parse json")
+				continue
+			}
+			handler(v)
+			msg.Ack(false)
+		}
+	}()
+
+	return nil
+}
+
 func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
 	ch, err := conn.Channel()
 	if err != nil {
