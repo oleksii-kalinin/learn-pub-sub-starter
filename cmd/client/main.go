915489e5b772faea966ccf7f8b723c45f4ca9d3d
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-05 10:36:20 +0100

    @codereabbitai (#2)
    * Add basic connection and pubsub
    
    * Add loop for server repl
    
    * Update server
    
    * Update cmd/server/main.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update cmd/client/main.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update cmd/server/main.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update main.go
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 
 func main() {
+	client, err := amqp.Dial(os.Getenv("AMQP_URL"))
+	FailOnError(err, "Failed to connect to AMQP")
+	defer func(client *amqp.Connection) {
+		err = client.Close()
+		FailOnError(err, "Unable to close AMQP connection")
+	}(client)
 	fmt.Println("Starting Peril client...")
+
+	msg, err := gamelogic.ClientWelcome()
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, msg)
+
+	ch, _, err := pubsub.DeclareAndBind(client, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.TransientQueue)
+	defer func(ch *amqp.Channel) {
+		ch.Close()
+	}(ch)
+
+	state := gamelogic.NewGameState(msg)
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "spawn":
+			err = state.CommandSpawn(words)
+			if err != nil {
+				log.Println(err)
+			}
+		case "move":
+			_, err = state.CommandMove(words)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Println("move ok")
+		case "status":
+			state.CommandStatus()
+		case "spam":
+			log.Println("no spam!")
+		case "quit":
+			gamelogic.PrintQuit()
+			//break StateLoop
+			return
+		default:
+			log.Println("unknown command")
+		}
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 
 func main() {
+	conn, err := amqp.Dial(os.Getenv("AMQP_URL"))
+	FailOnError(err, "Failed to connect to AMQP")
+	defer func(conn *amqp.Connection) {
+		err = conn.Close()
+		if err != nil {
+			log.Printf("Warning: Unable to close AMQP connection: %s", err)
+		}
+	}(conn)
+
+	routingKey := fmt.Sprintf("%s.*", routing.GameLogSlug)
+
+	_, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilTopic, routing.GameLogSlug, routingKey, pubsub.DurableQueue)
+	if err != nil {
+		log.Fatalf("could not declare and bind game logs queue: %v", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
+	publishCh, err := conn.Channel()
+	FailOnError(err, "Error opening channel")
+	defer publishCh.Close()
 	fmt.Println("Starting Peril server...")
+
+	gamelogic.PrintServerHelp()
+	for {
+		input := gamelogic.GetInput()
+		if len(input) <= 0 {
+			continue
+		}
+		switch input[0] {
+		case "pause":
+			log.Println("sending pause message")
+			err = pubsub.PublishJSON(publishCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+			FailOnError(err, "")
+
+		case "resume":
+			log.Println("sending resume message")
+			err = pubsub.PublishJSON(publishCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+			FailOnError(err, "")
+
+		case "quit":
+			log.Println("quitting")
+			return
+		default:
+			log.Println("unknown command")
+			break
+		}
+	}
 }
