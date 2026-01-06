5518a42ec5e7fb382406ae2ea67850ee16cde32f
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-06 14:28:04 +0100

    1

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/oleksii-kalinin/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/oleksii-kalinin/learn-pub-sub-starter/internal/pubsub"
 	"github.com/oleksii-kalinin/learn-pub-sub-starter/internal/routing"
+	"github.com/rabbitmq/amqp091-go"
 )
 
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.AckType {
+func handlerPause(gs *gamelogic.GameState, _ *amqp091.Channel) func(routing.PlayingState) pubsub.AckType {
 	return func(playingState routing.PlayingState) pubsub.AckType {
 		defer fmt.Print("> ")
 		gs.HandlePause(playingState)
@@ -16,15 +18,44 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Ack
 	}
 }
 
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckType {
+func handlerMove(gs *gamelogic.GameState, channel *amqp091.Channel) func(gamelogic.ArmyMove) pubsub.AckType {
 	return func(move gamelogic.ArmyMove) pubsub.AckType {
+		var err error
 		defer fmt.Print("> ")
 		outcome := gs.HandleMove(move)
 		switch outcome {
-		case gamelogic.MoveOutComeSafe, gamelogic.MoveOutcomeMakeWar:
+		case gamelogic.MoveOutComeSafe:
 			return pubsub.Ack
 		case gamelogic.MoveOutcomeSamePlayer:
 			return pubsub.NackDiscard
+		case gamelogic.MoveOutcomeMakeWar:
+			err = pubsub.PublishJSON(channel, routing.ExchangePerilTopic, routing.WarRecognitionsPrefix+"."+gs.GetUsername(), gamelogic.RecognitionOfWar{
+				Attacker: move.Player,
+				Defender: gs.GetPlayerSnap(),
+			})
+			if err != nil {
+				log.Println(err)
+				return pubsub.NackRequeue
+			}
+			return pubsub.Ack
+		default:
+			return pubsub.NackDiscard
+		}
+	}
+}
+
+func handlerWar(gs *gamelogic.GameState) func(msg gamelogic.RecognitionOfWar) pubsub.AckType {
+	// var err error
+	return func(msg gamelogic.RecognitionOfWar) pubsub.AckType {
+		defer fmt.Print("> ")
+		outcome, _, _ := gs.HandleWar(msg)
+		switch outcome {
+		case gamelogic.WarOutcomeNotInvolved:
+			return pubsub.NackRequeue
+		case gamelogic.WarOutcomeNoUnits:
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon, gamelogic.WarOutcomeYouWon, gamelogic.WarOutcomeDraw:
+			return pubsub.Ack
 		default:
 			return pubsub.NackDiscard
 		}
